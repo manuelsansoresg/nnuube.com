9e932979b9b67f4724425942c701903c7d463ed3
manuelsansores <manuelsansoresg@gmail.com>
2022-04-22 23:02:18 -0500

    random titulo

--- resources/js/components/star.js
+++ resources/js/components/star.js
@@ -1,41 +1,44 @@
 $(function () {
-
-    $("#rateYo").rateYo({
-        rating: 0,
-        fullStar: true,
-        onSet: function (rating, rateYoInstance) {
-            let title_id = $('#title_id').val();
-            rate(title_id, rating);
-        }
-    });
-
-    var rateMyTitle = $('#rateMyTitle').val();
-
-    $("#rateTitle").rateYo({
-        rating: rateMyTitle,
-        fullStar: true,
-        readOnly: true
-        
-    });
-    
-    window.rate = function (title_id, rate)
-    {
-        axios.get('/titulo/' + title_id + '/' + rate+'/rate')
-        .then(function (response) {
-            let result = response.data;
-            console.log(result);
-            Livewire.emit('setRate', title_id)
-
-        })
-        .catch(function (err) {
-            console.log(err);
+    if (document.getElementById('rateYo')) {
+        $("#rateYo").rateYo({
+            rating: 0,
+            fullStar: true,
+            onSet: function (rating, rateYoInstance) {
+                let title_id = $('#title_id').val();
+                rate(title_id, rating);
+            }
         });
 
+        var rateMyTitle = $('#rateMyTitle').val();
+
+        $("#rateTitle").rateYo({
+            rating: rateMyTitle,
+            fullStar: true,
+            readOnly: true
+
+        });
+
+        window.rate = function (title_id, rate) {
+            axios.get('/titulo/' + title_id + '/' + rate + '/rate')
+                .then(function (response) {
+                    let result = response.data;
+                    console.log(result);
+                    Livewire.emit('setRate', title_id)
+
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+
+        }
+
+        window.Livewire.on('setRateJavascript', rate => {
+            $("#rateTitle").rateYo("option", "rating", rate);
+        })
     }
-    
-    window.Livewire.on('setRateJavascript', rate => {
-        $("#rateTitle").rateYo("option", "rating", rate);
-    })
-    
+
+
+
+
 
 });
